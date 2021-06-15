2132db4da08e493b7d01c5187144c49ec6ba75c4
mgolshann <m_golshan85@yahoo.com>
2021-06-15 05:35:53 -0700

    create counter test with jest and enzyme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 }
+  }
+
+  render() {
+    return (
+      <div data-test='component-app'>
+        <h1 data-test="display-counter">the counter is currently {this.state.counter}</h1>
+        <button data-test="increment-button">increament</button>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,44 @@
-import { render, screen } from '@testing-library/react';
+
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new Adapter() });
+
+const setup = (props = {}, state = null) => {
+  const wrapper = shallow(<App {...props} />);
+  if (state) wrapper.setState(state);
+  return wrapper;
+}
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+}
+
+// if your contect render correctly or not
+test("renders without error", () => {
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'component-app');
+  expect(appComponent.length).toBe(1);
 });
+
+// if increament button exist or not
+test("render button increament exist or not", () => {
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, 'increment-button');
+  expect(button.length).toBe(1);
+});
+
+// if display counter exits or not
+test("renders counter display", () => {
+  const wrapper = setup();
+  const display = findByTestAttr(wrapper, 'display-counter');
+  expect(display.length).toBe(1)
+});
+
+// if counter start with Zero or not
+test("renders counter start form Zero", () => {
+  const wrapper = setup();
+  const counter = wrapper.state('counter');
+  expect(counter).toBe(0);
+});
+
